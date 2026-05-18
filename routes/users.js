46cf04aad9f563627a467fb92d2456dc4e523d08
ginorojo <ginorojoj@gmail.com>
2026-05-18 18:50:49 -0400

    mvc patterns

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,7 @@
 const router = require('express').Router();
-const mongodb = require('../data/database');
+const usersController = require('../controllers/users');
 
-router.get('/', async (req, res) => {
-  try {
-    const result = await mongodb
-      .getDatabase()
-      .db('project1')
-      .collection('users')
-      .find();
-
-    const users = await result.toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
 
 module.exports = router;
