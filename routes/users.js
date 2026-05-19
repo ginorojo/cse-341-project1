29d1855fac818fe152d024e3d17eefd7e6b18347
ginorojo <ginorojoj@gmail.com>
2026-05-18 20:21:09 -0400

    changes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,38 @@
 const router = require('express').Router();
 const usersController = require('../controllers/users');
 
+// #swagger.tags = ['Users']
 router.get('/', usersController.getAll);
+// #swagger.tags = ['Users']
+// #swagger.parameters['id'] = { in: 'path', required: true, type: 'string' }
 router.get('/:id', usersController.getSingle);
+// #swagger.tags = ['Users']
+// #swagger.parameters['body'] = {
+// #swagger.in = 'body'
+// #swagger.schema = {
+//   firstName: 'Gino',
+//   lastName: 'Rojo',
+//   email: 'gino@email.com',
+//   favoriteColor: 'blue',
+//   birthday: '2000-01-01'
+// }
+// }
 router.post('/', usersController.createUsers);
+// #swagger.tags = ['Users']
+// #swagger.parameters['id'] = { in: 'path', required: true, type: 'string' }
+// #swagger.parameters['body'] = {
+// #swagger.in = 'body'
+// #swagger.schema = {
+//   firstName: 'Gino',
+//   lastName: 'Rojo',
+//   email: 'gino@email.com',
+//   favoriteColor: 'blue',
+//   birthday: '2000-01-01'
+// }
+// }
 router.put('/:id', usersController.updateUsers);
+// #swagger.tags = ['Users']
+// #swagger.parameters['id'] = { in: 'path', required: true, type: 'string' }
 router.delete('/:id', usersController.deleteUsers);
 
 
